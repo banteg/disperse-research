b4c91fc1f411f70f86684648d03b3206d2d86e58
banteg <banteeg@gmail.com>
2018-11-12 13:33:24 +0700

    add accounts helper

---
+++ token_research/utils/accounts.py
@@ -0,0 +1,14 @@
+from eth_keys import KeyAPI
+
+backend = KeyAPI()
+accounts = {}
+
+
+def priv(n: int):
+    if n not in accounts:
+        accounts[n] = backend.PrivateKey(n.to_bytes(32, 'big'))
+    return accounts[n]
+
+
+def addr(n: int):
+    return priv(n).public_key.to_canonical_address()
