a1d51c3004ecc745be3534a0dd7ba5f91c2a9a8c
banteg <banteeg@gmail.com>
2018-11-12 17:26:14 +0700

    fix random accounts generator

--- token_research/utils/recipients.py
+++ token_research/utils/recipients.py
@@ -1,12 +1,11 @@
-from token_research.utils import accounts
+import os
+from token_research.utils.accounts import backend
 
 
 def generate_recipients(count):
     recipients = []
-    n = 100
     while len(recipients) < count:
-        n += 1
-        address = accounts.addr(n)
+        address = backend.PrivateKey(os.urandom(32)).public_key.to_canonical_address()
         # data field: zero = 4 gas, non-zero = 68 gas, aim for the worst case
         if address.count(b'\x00') == 0:
             recipients.append(address)
